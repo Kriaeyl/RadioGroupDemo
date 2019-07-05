75a64fd8cfe3e11b00be1fa937596e1f2c1a99b2
18055124 <Kriaeyl>
2019-07-05 15:08:02 +0800

    UwU

--- app/src/main/java/sg/edu/rp/dmsd/radiogroupdemo/MainActivity.java
+++ app/src/main/java/sg/edu/rp/dmsd/radiogroupdemo/MainActivity.java
@@ -13,8 +13,6 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     RadioGroup rg1;
-    RadioButton rb1;
-    RadioButton rb2;
     Button b1;
     TextView tv1;
 
@@ -24,20 +22,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         rg1 = findViewById(R.id.rg1);
-        rb1 = findViewById(R.id.radioButton);
-        rb2 = findViewById(R.id.radioButton2);
         tv1 = findViewById(R.id.textView);
         b1 = findViewById(R.id.button3);
 
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                RadioButton bb = findViewById(rg1.getCheckedRadioButtonId());
+                tv1.setText(bb.getText().toString() + " is selected");
                 SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
                 SharedPreferences.Editor prefEdit = prefs.edit();
                 prefEdit.putInt("gender", rg1.getCheckedRadioButtonId());
+                prefEdit.putString("text", tv1.getText().toString());
                 prefEdit.commit();
-                RadioButton bb = findViewById(rg1.getCheckedRadioButtonId());
-                tv1.setText(bb.getText().toString() + " is selected");
+
             }
         });
 
@@ -47,5 +45,6 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
         rg1.check(prefs.getInt("gender", 0));
+        tv1.setText(prefs.getString("text", ""));
     }
 }
